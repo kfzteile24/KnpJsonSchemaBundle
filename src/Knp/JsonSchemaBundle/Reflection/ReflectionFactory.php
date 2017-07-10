fed01545f36c2aaa48d1e979ade9ba71871675ad
Alberto Assmann <newLoki@users.noreply.github.com>
2017-07-10 08:38:18 +0200

    ignore Traits and Interfaces in the reflection factory (#1)

--- src/Knp/JsonSchemaBundle/Reflection/ReflectionFactory.php
+++ src/Knp/JsonSchemaBundle/Reflection/ReflectionFactory.php
@@ -24,15 +24,21 @@ class ReflectionFactory
             return array();
         }
 
-        $this->finder->files();
-        $this->finder->name('*.php');
-        $this->finder->in($directory);
+        $finder = clone $this->finder;
+        $finder->files();
+        $finder->name('*.php');
+        $finder->in($directory);
 
         $refClasses = array();
 
-        foreach ($this->finder->getIterator() as $name) {
+        foreach ($finder->getIterator() as $name) {
             $baseName      = substr($name, strlen($directory)+1, -4);
             $baseClassName = str_replace('/', '\\', $baseName);
+
+            if (preg_match('/Interface/', $baseClassName) > 0 || preg_match('/Trait/', $baseClassName) > 0) {
+                continue;
+            }
+
             $refClasses[]  = $this->create($namespace.'\\'.$baseClassName);
         }
 
